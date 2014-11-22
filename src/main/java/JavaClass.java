937367d69b3ffb0a49df46be9fbf8596e038607a
Hans Peeter Tulmin <soulyyy@gmail.com>
2014-11-23 00:07:31 +0200

    Removed junk

--- src/main/java/JavaClass.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Hans Peeter Tulmin on 22.11.2014.
- */
-public class JavaClass {
-
-	public static String printStuff() {
-		return "Tere, siin on Java";
-	}
-}
--- src/main/scala/Main.scala
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Hans Peeter Tulmin on 22.11.2014.
- */
-object Main {
-  def main (args: Array[String]): Unit = {
-    println(JavaClass.printStuff())
-  }
-}
