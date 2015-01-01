4c82c9eb7dd927ecf41837d292ac70dde1f5926c
Hans <soulyyy@gmail.com>
2015-01-02 00:14:53 +0200

    draft

---
+++ src/main/java/controllers/GameServlet.java
@@ -0,0 +1,7 @@
+package controllers;
+
+/**
+ * Created by Hans on 1.01.2015.
+ */
+public class GameServlet {
+}
---
+++ src/main/scala/game/Board.scala
@@ -0,0 +1,8 @@
+package game
+
+/**
+ * Created by Hans on 1.01.2015.
+ */
+class Board {
+
+}
